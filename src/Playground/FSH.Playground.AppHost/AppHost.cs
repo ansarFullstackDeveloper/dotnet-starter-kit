IamFlashUser/dotnet-webapi-starter-kit

feat: restructure admin app as pnpm monorepo and wire into Aspire via JavaScript hosting
- Move clients/apps/admin → clients/admin with workspace packages
  (api-client, auth, ui) extracted into clients/admin/packages/
- Bump Aspire hosting packages to 13.2.1, add Aspire.Hosting.JavaScript
- Wire fsh-admin Next.js app into AppHost with npm dev resource,
  API reference, and FSH_API_URL env injection
- Fix .gitignore: exempt clients/**/packages/ from the **/[Pp]ackages/*
  NuGet rule so the Next.js workspace packages are tracked

Co-Authored-By: Claude Opus 4.6 (1M context) <noreply@anthropic.com>

src/Playground/FSH.Starter.AppHost/AppHost.cs
@@ -19,7 +19,7 @@ var redisConnectionString = ReferenceExpression.Create(
     $"{redisPlainTcp.Property(EndpointProperty.HostAndPort)},password={redis.Resource.PasswordParameter!}");
 
 // API Service
-builder.AddProject<Projects.FSH_Starter_Api>("fsh-api")
+var api = builder.AddProject<Projects.FSH_Starter_Api>("fsh-api")
     .WithReference(postgres)
     .WithReference(redis)
     .WaitFor(postgres)
@@ -31,5 +31,13 @@ builder.AddProject<Projects.FSH_Starter_Api>("fsh-api")
     .WithEnvironment("CachingOptions__Redis", redisConnectionString)
     .WithEnvironment("CachingOptions__EnableSsl", "false");
 
+// Admin App (Next.js)
+builder.AddJavaScriptApp("fsh-admin", "../../clients/admin", "dev")
+    .WithNpm()
+    .WithReference(api)
+    .WaitFor(api)
+    .WithHttpEndpoint(port: 3000, env: "PORT")
+    .WithExternalHttpEndpoints()
+    .WithEnvironment("FSH_API_URL", api.GetEndpoint("http"));
 
 await builder.Build().RunAsync();

ansarFullstackDeveloper/dotnet-starter-kit

src/Playground/FSH.Playground.AppHost/AppHost.cs
@@ -13,7 +13,8 @@ var redisPlainTcp = redis.GetEndpoint("secondary");
 var redisConnectionString = ReferenceExpression.Create(
     $"{redisPlainTcp.Property(EndpointProperty.HostAndPort)},password={redis.Resource.PasswordParameter!}");
 
-builder.AddProject<Projects.Playground_Api>("playground-api")
+// API Service
+var api = builder.AddProject<Projects.Playground_Api>("playground-api")
     .WithReference(postgres)
     .WithEnvironment("ASPNETCORE_ENVIRONMENT", "Development")
     .WithEnvironment("OpenTelemetryOptions__Exporter__Otlp__Endpoint", "https://localhost:4317")
@@ -28,6 +29,16 @@ builder.AddProject<Projects.Playground_Api>("playground-api")
     .WithEnvironment("CachingOptions__EnableSsl", "false")
     .WaitFor(redis);
 
+// Blazor UI
 builder.AddProject<Projects.Playground_Blazor>("playground-blazor");
+
+// Admin App (Next.js)
+builder.AddJavaScriptApp("fsh-admin", "../../clients/admin", "dev")
+    .WithNpm()
+    .WithReference(api)
+    .WaitFor(api)
+    .WithHttpEndpoint(port: 3000, env: "PORT")
+    .WithExternalHttpEndpoints()
+    .WithEnvironment("FSH_API_URL", api.GetEndpoint("http"));
 
 await builder.Build().RunAsync();